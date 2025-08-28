920b06d272a4b08ae7d97059c9cc26c211dcf624
Guru Kasinath <123kasi456789@gmail.com>
2025-08-28 16:58:54 +0530

    Updated Assignment Q 1

---
+++ Operators & List/1Answer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+numbers=(1,2,3,4,5,6,7,8,9,10)
+print("first element:",numbers[0])
+print("last element:",numbers[-1])
+middle_index = len(numbers) //2
+print("middle element:", numbers[middle_index])
+
+
+# In[ ]:
+
+
+
+
